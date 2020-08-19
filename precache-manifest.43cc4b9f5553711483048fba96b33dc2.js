242a2f4a3cd9d4bc8ee4d130ae1373f3ee96e21f
waseemahmad01 <wa031522@gmail.com>
2020-08-19 10:24:27 +0500

    Updates

---
+++ precache-manifest.43cc4b9f5553711483048fba96b33dc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b93fb8afb157e8e2ae10d4ba32ef3d1",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "5d30d62167e318bb2eb3",
+    "url": "/todo-app/static/css/main.499dfa52.chunk.css"
+  },
+  {
+    "revision": "658b672393ba4ab55200",
+    "url": "/todo-app/static/js/2.eed614d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.eed614d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d30d62167e318bb2eb3",
+    "url": "/todo-app/static/js/main.0943865a.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
